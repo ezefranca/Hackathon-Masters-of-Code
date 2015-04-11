8ddfa5c1ac3ae77247cb1b74b4f913df9273520a
RodolfoF <rodolfo@minhavida.com.br>
2015-04-11 19:04:05 -0300

    adicao de cartao ok

--- web/HackAPI/HackAPI/Models/Cartao.cs
+++ web/HackAPI/HackAPI/Models/Cartao.cs
@@ -1,12 +1,13 @@
 ﻿using Antlr.Runtime.Tree;
+using Newtonsoft.Json;
 
 namespace HackAPI.Models
 {
-   public class Cartao
+    public class Cartao
     {
         public int Id { get; set; }
-        public string Token{ get; set; }
-
+        public string Token { get; set; }
+        [JsonIgnore]
         public virtual Cadastro Dono { get; set; }
     }
 }
